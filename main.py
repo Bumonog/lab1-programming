3ec1d087e9c15ae717c376cdf86aa8eebe806db8
AdamSendler <330973865+Bumonog@users.noreply.github.com>
2026-09-26 18:54:38 +0300

    lab3_task3(Python): InitialCommit

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-a, b  = float(input("Введите первое число: ")), float(input("Введите второе число: "))
+seconds = int(input("введите кличество секунд: "))
 
-print(f'Сумма: {a + b}')
-print(f'Разность: {a - b}')
-print(f'ПРоизведение: {a * b}')
-print(f'Деление(2 знака после запятой): {a / b:.2f}')
-print(f'Целочисленное деление: {a // b}')
-print(f'Остаток: {a % b}')
+hours = seconds // 3600
+minutes = (seconds % 3600) // 60
+seconds = seconds % 60
 
-try:
-    print(f'Степнь: {a ** b:.2f}')
-except OverflowError:
-    print("Степень: cлишком большой результат.")
+print(f"Часы: {hours:02d} минуты: {minutes:02d} секунды: {seconds:02d}")
