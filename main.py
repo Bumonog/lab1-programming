1a15fee29b94bf6357e846b0ab32b9f9e971c49c
AdamSendler <330973865+Bumonog@users.noreply.github.com>
2026-09-26 11:31:13 +0300

    lab3_task1: InitialCommit

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-text = 'World'
-print(f"Hello, {text}!")
+# Запрос данных с "Защитой от дурака"
+while True:
+    try:
+        num_integer = int(input("Введите целое число:"))
+        break
+    
+    except ValueError:
+        print("Ошибка! Введите целое число!")
+        
+while True:
+    try:
+        num_float = float(input("Введите дробное число:"))
+        break
+    except ValueError:
+        print("Ошибка! Введите дробное число!")
+
+text = input("Введите строку:")
+
+# Вывод типов данных переменных и их значений
+print(f"Значение: {num_integer}, тип:{type(num_integer).__name__}")
+print(f"Значение:{num_float}, тип:{type(num_float).__name__}")
+print(f"Значение:{text}, тип:{type(text).__name__}")
